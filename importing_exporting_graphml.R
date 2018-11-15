c7b62be54fef8cc21fc755be86523d42d7cbc1a7
Yevgeniy Golovchenko <golov90@gmail.com>
2018-11-15 18:22:10 +0100

    Add files via upload

--- importing_exporting_graphml.R
+++ importing_exporting_graphml.R
@@ -3,6 +3,15 @@ library(igraph)
 #exporting the rts.g graph object as a graphml file 
 write.graph(rts.g, file="your_file.graphml", format="graphml") #rts.g is your graph file
 
-##reading a graphml file as a graph object
+  
+### you can import the network file in gdf format from Youtube Data Tools using readgdf() 
+### Alternatively, you can covert the gdf file to graphml by going through the following steps:
+#1  Read the gdf file in Gephi
+#2 "Export" the network as graphml file into your working directory
+#3  Load the graphml file in R using the code below
+
+##reading a graphml file in your working directory as a graph object
 g_import <- read.graph("your_file_in_the_working_dirctory.graphml", format = "graphml") 
 
+### test run 
+plot.igraph(g_import)
